598511d214433117715cfdc0af73bb5da3f30b23
Greg Stickel <gregory.stickel@live.com>
2022-11-10 16:20:56 -0500

    Updated 4:20pm 11-10-2022

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 //import logo from './logo.svg';
-import './App.css';
-import Square from './Square';
+import "./App.css";
+import Square from "./Square";
 
 function App() {
   //let str = "This is a string";
-  const [squares, setSquares] =  useState(["","","","","","","","",""]);
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
+
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""]);
+    setPlayer(true);
+  }
 
   return (
     <div className="App">
-      {
-      <Square 
-        squares={squares}
-        setSquares={setSquares}
-        player={player}
-        setPlayer={setPlayer}
-      />
-      }
+      <div className="container">
+        {squares.map((val, index) => {
+          return (
+            <Square
+              setSquares={setSquares}
+              index={index}
+              squareValue={val}
+              squares={squares}
+              player={player}
+              setPlayer={setPlayer}
+            />
+          );
+        })}
+      </div>
+      <button onClick={handleClick}>Reset</button>
     </div>
   );
 }
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 
+const handleClick = (props) => {
+    if (!props.squareValue){
+        props.squares.splice(props.index, 1, "X");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+    } else {
+        props.squares.splice(props.index, 1, "O");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+    }
+
+}
+
+
 const Square = (props) => {
     console.log(props.squares, props.player);
     return (
         
-        <div className="square"></div>
+        <div className="square" onClick={handleClick}>
+            {props.squareValue === "O" ? <img alt="Dev Mountain Circle" src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" /> : props.squareValue}
+
+        </div>
     );
 };
 
